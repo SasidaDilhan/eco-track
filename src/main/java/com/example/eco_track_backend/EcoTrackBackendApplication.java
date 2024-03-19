a67a8247c4927558c2d410fc86f03f38b5f64496
IT21158254 <IT21158254@my.sliit.lk>
2024-03-19 14:19:22 +0530

    initial commit

---
+++ src/main/java/com/example/eco_track_backend/EcoTrackBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eco_track_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoTrackBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoTrackBackendApplication.class, args);
+	}
+
+}
