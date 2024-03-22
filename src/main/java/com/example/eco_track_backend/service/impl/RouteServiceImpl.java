47b363e7e15ac74e14bff234def2a616f92629dc
IT21158254 <IT21158254@my.sliit.lk>
2024-03-22 17:05:23 +0530

    routes adding

---
+++ src/main/java/com/example/eco_track_backend/service/impl/RouteServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.eco_track_backend.service.impl;
+
+import com.example.eco_track_backend.service.RouteService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RouteServiceImpl implements RouteService {
+}
