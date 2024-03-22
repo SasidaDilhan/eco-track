4d3293c8c169402c7e7c428d2b2b99213bb1e9df
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2024-03-22 19:07:24 +0530

    store model add

---
+++ src/main/java/com/example/eco_track_backend/model/Store.java
@@ -0,0 +1,19 @@
+package com.example.eco_track_backend.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name ="stores")
+public class Store {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private Long quantity;
+
+    @ManyToOne
+    private User user;
+}
--- src/main/java/com/example/eco_track_backend/model/User.java
+++ src/main/java/com/example/eco_track_backend/model/User.java
@@ -35,6 +35,9 @@ public class User implements UserDetails {
     @OneToMany(mappedBy = "user")
     private List<Notice> noticeList;
 
+    @OneToMany(mappedBy = "user")
+    private List<Store> storeList;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         List<SimpleGrantedAuthority> roles = new ArrayList<>();
