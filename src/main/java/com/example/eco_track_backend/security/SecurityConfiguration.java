e51132bc0cf7c8b2cde5b5ec765bbe89be0a0228
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2024-03-23 12:11:01 +0530

    specific user all items get done

--- src/main/java/com/example/eco_track_backend/security/SecurityConfiguration.java
+++ src/main/java/com/example/eco_track_backend/security/SecurityConfiguration.java
@@ -38,6 +38,7 @@ public class SecurityConfiguration {
                     auth.requestMatchers(antMatchers(HttpMethod.POST, "/routes/{route_id}/disposalPlaces")).permitAll();
                     auth.requestMatchers(antMatchers(HttpMethod.POST, "routes")).permitAll();
                     auth.requestMatchers(antMatchers(HttpMethod.GET, "routes/users/store_items")).permitAll();
+                    auth.requestMatchers(antMatchers(HttpMethod.GET, "/users/{user_id}/store_items")).permitAll();
 
                     auth.requestMatchers(antMatchers(HttpMethod.POST, "/")).permitAll();
                     auth.requestMatchers(antMatchers(HttpMethod.POST, "/register")).permitAll();
