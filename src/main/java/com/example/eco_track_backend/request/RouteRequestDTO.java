bf225ed5df08b45f82206bd7c5268100e65879b7
IT21158254 <IT21158254@my.sliit.lk>
2024-03-23 12:48:05 +0530

    validations added

--- src/main/java/com/example/eco_track_backend/request/RouteRequestDTO.java
+++ src/main/java/com/example/eco_track_backend/request/RouteRequestDTO.java
@@ -1,11 +1,24 @@
 package com.example.eco_track_backend.request;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RouteRequestDTO {
 
+    @NotBlank(message = "Name cannot be blank")
     private String name;
-    private double latitude;
-    private double longitude;
+
+    @NotNull(message = "Latitude cannot be null")
+    @DecimalMin(value = "-90.0", message = "Latitude must be at least -90.0")
+    @DecimalMax(value = "90.0", message = "Latitude must be at most 90.0")
+    private Double latitude;
+
+    @NotNull(message = "Longitude cannot be null")
+    @DecimalMin(value = "-180.0", message = "Longitude must be at least -180.0")
+    @DecimalMax(value = "180.0", message = "Longitude must be at most 180.0")
+    private Double longitude;
 }
