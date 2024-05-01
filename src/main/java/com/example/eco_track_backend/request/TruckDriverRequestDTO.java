7bafd77fe70fda3608900c1946839c5ddb1dde59
purnalakshitha99 <purnalakshitha2@gmail.com>
2024-05-02 00:26:21 +0530

    added the new column to truck driveer

--- src/main/java/com/example/eco_track_backend/request/TruckDriverRequestDTO.java
+++ src/main/java/com/example/eco_track_backend/request/TruckDriverRequestDTO.java
@@ -39,4 +39,11 @@ public class TruckDriverRequestDTO {
     @NotNull(message = "Role cannot be null")
     @Enumerated(EnumType.STRING)
     private ROLES role;
+
+
+    private String addressLine1;
+    private String addressLine2;
+    private String city;
+    private String state;
+    private Long zip;
 }
